77a0d542096e3c27aedf2a0b20a498e6efa9c9fd
Luís Alves <24353152+alves-luis@users.noreply.github.com>
2018-11-19 22:21:33 +0000

    Atualização aos relacionamentos no relatório
    E adicionado .sql para povoar

---
+++ SQL/Povoamento.sql
@@ -0,0 +1,6 @@
+-- Base de Dados do Trabalho
+use parqueaquatico;
+
+-- Povoamento da Tabela atracao
+insert into Atracao (Id, Designacao, Zona, Capacidade, Duracao, Altura_Minima) values
+	(1,"Big Show","Cabeça",32,"00:3:10",170);
