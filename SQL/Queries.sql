12377ff083bd3767386a1198c3ae6ecc1e6ba558
alves-luis <24353152+alves-luis@users.noreply.github.com>
2018-11-21 15:48:04 +0000

    Changed from Procedure to Function 2 queries (makes more sense

--- SQL/Queries.sql
+++ SQL/Queries.sql
@@ -17,28 +17,36 @@ END //
 -- dos utilizadores de uma atração num intervalo de tempo;
 DELIMITER //
 
-DROP PROCEDURE IF EXISTS averageWait //
+DROP FUNCTION IF EXISTS averageWait //
 
-CREATE PROCEDURE averageWait(id INT, fromWhen DATETIME, toWhen DATETIME)
+CREATE FUNCTION averageWait(id INT, fromWhen DATETIME, toWhen DATETIME)
+	RETURNS TIME
+    DETERMINISTIC
 BEGIN
-    SELECT sec_to_time(avg(time_to_sec(timediff(V.Data_entrada_atracao,V.Data_entrada_fila)))) as "Média" from e_visitada_por as V
+	DECLARE result TIME(0);
+	SELECT sec_to_time(avg(time_to_sec(timediff(V.Data_entrada_atracao,V.Data_entrada_fila)))) as "Média" from e_visitada_por as V
 	WHERE (V.Data_entrada_fila between fromWhen and toWhen) 
 		and id = V.Atracao_Id
         and (V.Data_entrada_atracao between fromWhen and toWhen)
-        and V.Data_entrada_atracao is not null;
+        and (V.Data_entrada_atracao is not null)
+        into result;
+	RETURN result;
 END //
 
 -- 3. Obter o número de utilizadores em fila numa atração 
 -- num intervalo de tempo;
 DELIMITER //
 
-DROP PROCEDURE IF EXISTS countWaiting //
+DROP FUNCTION IF EXISTS countWaiting //
 
-CREATE PROCEDURE countWaiting(id INT, fromWhen DATETIME, toWhen DATETIME)
+CREATE FUNCTION countWaiting(idAtracao INT, whenWait DATETIME)
+	RETURNS INT
+    DETERMINISTIC
 BEGIN
+	DECLARE result INT;
 	SELECT count(V.Utilizador_Id) as "Nº de utilizadores em espera" from e_visitada_por as V
     where (V.data_entrada_atracao = null)
-    and id = V.Atracao_Id
-    and (V.Data_entrada_atracao between fromWhen and toWhen);
+    and idAtracao = V.Atracao_Id
+    and (V.Data_entrada_fila <= whenWait) into result;
+    RETURN result;
 END //
-
